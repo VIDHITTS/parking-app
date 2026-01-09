36769734c33eaabef04c76b55f9559061b9d4a9f
VIDHITTS <vidhitts@gmail.com>
2026-01-09 10:00:00 +0530

    Add car management features

---
+++ backend/src/routes/cars.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { addCar, getCarsByDriver, getAllCars, getCarById } from '../controllers/carController.js';
+
+const router = express.Router();
+
+router.post('/', addCar);
+router.get('/', getCarsByDriver);
+router.get('/all', getAllCars);
+router.get('/:id', getCarById);
+
+export default router;
